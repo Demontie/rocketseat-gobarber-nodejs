22ee1cb0970ef31ec20fe26994cd60b454451d51
Demontie Costa <demontiecosta17@gmail.com>
2020-02-22 18:07:06 -0300

    Configuração de enviar o email concluida

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd2c376d6a24de7',
+    pass: 'c901097e5188e7',
+  },
+  default: {
+    from: 'Demontie da costa <noreply@gobarber.com>',
+  },
+};
