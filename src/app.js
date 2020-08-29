c42aef33f50464cda061c591cb5ceb4e0a1d45f8
Demontie <demontiecosta17@gmail.com>
2020-08-29 11:13:11 -0300

    comitado

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ class App {
   }
 
   exceptionHandler() {
-    this.server.use(async (err, req, res, next) => {
+    this.server.use(async (err, req, res) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
         return res.status(500).json(errors);
